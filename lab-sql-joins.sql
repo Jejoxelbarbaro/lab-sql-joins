7ef017b6c4650106db3d69dbb8caac287cba544f
JERONIMO CAGLIERO <98931849+Jejoxelbarbaro@users.noreply.github.com>
2026-07-14 17:41:32 +0200

    Join Lab

---
+++ lab-sql-joins.sql
@@ -0,0 +1,43 @@
+USE Sakila
+
+-- 1 films per category
+SELECT 
+	category_id,
+    COUNT(film_id) AS number_of_films
+FROM  film_category
+GROUP BY category_id;
+
+-- 2 STORE location
+SELECT
+	store.store_id,
+    address.address,
+    city.city,
+    country.country
+FROM store
+JOIN address
+	ON store.address_id = address.address_id
+JOIN city
+	ON address.city_id = city.city_id
+JOIN country
+	ON city.country_id = country.country_id;
+   
+-- 3 Revenue per store
+SELECT
+	customer.store_id,
+    SUM(payment.amount) AS revenue
+FROM customer
+JOIN payment
+	ON customer.customer_id = payment.customer_id
+GROUP BY customer.store_id;
+    
+-- 4 AVG running time of fils 4 each cat
+
+SELECT
+	category.name,
+	ROUND(AVG(film.length)) AS avg_runing_time
+FROM film_category
+JOIN film
+	ON film_category.film_id = film.film_id
+JOIN category
+	ON film_category.category_id= category.category_id 
+GROUP BY category.name;
